46f7ad92458ddd57779e04370b514c612bdfa406
EsRoHi26 <es.rojas@estudiantec.cr>
2025-10-14 19:10:31 -0600

    procesos tiny update

--- frontend/src/pages/Admin/Procesos/Procesos.jsx
+++ frontend/src/pages/Admin/Procesos/Procesos.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Accordion, AccordionTab } from 'primereact/accordion';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
-import { InputTextarea } from 'primereact/inputtextarea';
+import { FileUpload } from 'primereact/fileupload';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import SideBar from '../../../components/SideBar';
 import './Procesos.css';
+import { FloatLabel } from 'primereact/floatlabel';
+import AdvancedFilter from '../../../components/Table';
 
 const Procesos = () => {
     const [processes, setProcesses] = useState([
@@ -16,7 +18,7 @@ const Procesos = () => {
         { id: 3, name: 'Customer Feedback', description: 'Collect and analyze customer feedback', status: 'Active' },
     ]);
 
-    const [newProcess, setNewProcess] = useState({ name: '', description: '', status: 'Active' });
+    const [newProcess, setNewProcess] = useState({ name: '', periodo: '', unidad: '', responsable: '' });
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -53,19 +55,7 @@ const Procesos = () => {
                 <AccordionTab header="Lista de Procesos" className="procesos-tab">
                     <Card className="procesos-card">
                         <div className="card-content">
-                            <DataTable 
-                                value={processes} 
-                                responsiveLayout="scroll" 
-                                stripedRows
-                                className="full-height-table"
-                                scrollable
-                                scrollHeight="flex"
-                            >
-                                <Column field="id" header="ID" sortable style={{ width: '5rem' }} />
-                                <Column field="name" header="Nombre" sortable />
-                                <Column field="description" header="Descripción" />
-                                <Column field="status" header="Estado" sortable />
-                            </DataTable>
+                            <AdvancedFilter/>
                         </div>
                     </Card>
                 </AccordionTab>
@@ -74,32 +64,69 @@ const Procesos = () => {
                 <AccordionTab header="Crear Nuevo Proceso" className="procesos-tab">
                     <Card className="procesos-card">
                         <div className="card-content">
-                            <div className="form-group">
-                                <label htmlFor="name">Nombre del proceso</label>
-                                <InputText
-                                    id="name"
-                                    name="name"
-                                    value={newProcess.name}
-                                    onChange={handleInputChange}
-                                    placeholder="Ej. Aprobación de proyectos"
-                                    className="input-field"
-                                />
-                            </div>
+                            <div className="main-grid">
+                                <div className="form-group">
+                                    <FloatLabel>
+                                        <InputText
+                                            id="name"
+                                            name="name"
+                                            value={newProcess.name}
+                                            onChange={handleInputChange}
+                                            placeholder="Ej. Aprobación de proyectos"
+                                            className="input-field"
+                                        />
+                                        <label htmlFor="name">Nombre del proceso</label>
+                                    </FloatLabel>
 
-                            <div className="form-group">
-                                <label htmlFor="description">Descripción</label>
-                                <InputTextarea
-                                    id="description"
-                                    name="description"
-                                    value={newProcess.description}
-                                    onChange={handleInputChange}
-                                    rows={4}
-                                    placeholder="Describe brevemente el proceso"
-                                    className="input-field"
-                                />
-                            </div>
+                                    <FloatLabel>
+                                        <InputText
+                                            id="periodo"
+                                            name="periodo"
+                                            value={newProcess.periodo}
+                                            onChange={handleInputChange}
+                                            placeholder="Periodo"
+                                            className="input-field"
+                                        />
+                                        <label htmlFor="periodo">Periodo</label>
+                                    </FloatLabel>
 
-                            <Button label="Agregar Proceso" icon="pi pi-plus" onClick={handleAddProcess} className="add-button" />
+                                    <FloatLabel>
+                                        <InputText
+                                            id="unidad"
+                                            name="unidad"
+                                            value={newProcess.unidad}
+                                            onChange={handleInputChange}
+                                            placeholder="Unidad de medida"
+                                            className="input-field"
+                                        />
+                                        <label htmlFor="unidad">Unidad de medida</label>
+                                    </FloatLabel>
+
+                                    <FloatLabel>
+                                        <InputText
+                                            id="responsable"
+                                            name="responsable"
+                                            value={newProcess.responsable}
+                                            onChange={handleInputChange}
+                                            placeholder="Responsable"
+                                            className="input-field"
+                                        />
+                                        <label htmlFor="responsable">Responsable</label>
+                                    </FloatLabel>
+                                </div>
+
+                                <div className="form-group">
+                                    <FileUpload
+                                        name="demo[]"
+                                        url={'/api/upload'}
+                                        multiple
+                                        accept="image/*"
+                                        maxFileSize={1000000}
+                                        emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+                                </div>
+
+                                <Button label="Agregar Proceso" icon="pi pi-plus" onClick={handleAddProcess} className="add-button" />
+                            </div>
                         </div>
                     </Card>
                 </AccordionTab>
